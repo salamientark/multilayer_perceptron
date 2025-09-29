68268c83e36c0933d7a4f38c4688b1c1f935d4bc
Jay Moker <AC@B>
2025-09-29 18:08:38 +0200

    Updated split_dataset.py

--- split_dataset.py
+++ split_dataset.py
@@ -51,21 +51,17 @@ def parse_args():
     parser = ap.ArgumentParser(prog="split_dataset.py",
                                description="Split the dataset into a training "
                                            "set and a validation set.",
-                               usage="usage: split_dataset.py [-h] [--seed "
-                                     "SEED] [--train-ratio TRAIN_RATIO] "
-                                     "<dataset_path> [--outfile OUTFILE "
-                                     "[OUTFILE ...]]",
                                epilog=">^-^<")
 
     # Add parser option
+    parser.add_argument("--outfile", "-o", type=str,
+                        default="data_training.csv,data_validation.csv",
+                        help="output dataset name", nargs='+')
     parser.add_argument("--seed", "-s", type=int, default=1,
                         help="Random seed for shufling.")
     parser.add_argument("--train-ratio", "-r", type=float, default=0.8,
                         help="ratio of the training set size.")
     parser.add_argument("dataset_path", help="Path to the input csv file.")
-    parser.add_argument("--outfile", "-o", type=str,
-                        default="data_training.csv,data_validation.csv",
-                        help="output dataset name", nargs='+')
 
     # Parse program arguments
     args = parser.parse_args()
@@ -79,7 +75,8 @@ def parse_args():
 
     if len(args.outfile) == 1:
         outfiles = args.outfile[0].split(',')
-        outfiles = [file.strip() for file in outfiles if file.strip() != ""]
+        args.outfile = [file.strip() for file in outfiles
+                        if file.strip() != ""]
         if not len(outfiles) == 2:
             parser.error("The --outfile argument must contain 2 filenames.")
         if outfiles[0] == outfiles[1]:
@@ -114,6 +111,7 @@ def main(args):
       --outfiles <str,str> or <str> <str>   Output files
     """
     try:
+        print(args.outfile)
         # Get Dataframe
         df = pd.read_csv(args.dataset_path)
         df.columns = data_columns_names
