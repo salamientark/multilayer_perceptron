3076838d4476075cacc69d7868085a135dc6d7a7
Jay Moker <AC@B>
2025-09-29 14:13:26 +0200

    Parser V1

--- split_dataset.py
+++ split_dataset.py
@@ -1,7 +1,8 @@
-import sys
+import argparse as ap
 import pandas as pd
 
 
+# Data columns name
 data_columns_names = [
     "id",
     "diagnosis",
@@ -44,55 +45,59 @@ GREEN = '\033[92m'
 RESET = '\033[0m'
 
 
-def parser(ac: int, av: list) -> dict:
-    """Verify program argument validity raise Exception on Error
+def parse_args():
+    """Get program command line argument"""
+    # Init parser
+    parser = ap.ArgumentParser(prog="split_dataset.py",
+                               description="Split the dataset into a training "
+                                           "set and a validation set.",
+                               usage="usage: split_dataset.py [-h] [--seed "
+                                     "SEED] [--train-ratio TRAIN_RATIO] "
+                                     "<dataset_path> [--outfile OUTFILE "
+                                     "[OUTFILE ...]]",
+                               epilog=">^-^<")
 
-    Parameters:
-      ac (int): Program parameters len
-      av (list): Program options
-    """
-    option = {'seed': 1, 'ratio': 0.8}
-    if ac == 2:
-        return option
-    if ac == 4:
-        if av[1].strip() == "--seed":
-            if int(av[2]) <= 0:
-                raise Exception("--seed argument must be a positive integer")
-            option['seed'] = int(av[2])
-        elif av[1].strip() == "--train-ratio":
-            if float(av[2]) <= 0.0 or float(av[2]) >= 1.0:
-                raise Exception("--train-ratio argument must be a float"
-                                " ]0.0, 1.0[")
-            option['ratio'] = float(av[2])
-        else:
-            raise Exception(f"Unkown option \"{av[2]}\"")
-        return option
-    if av[1].strip() != "--seed" and av[1].strip() != "--train-ratio":
-        raise Exception(f"Unkown option \"{av[1]}\"")
-    if av[1].strip() == av[3].strip():
-        raise Exception("Realy!?")
-    if av[1].strip() == "--seed":
-        if int(av[2]) <= 0:
-            raise Exception("--seed argument must be a positive integer")
-        option['seed'] = int(av[2])
-    if av[1].strip() == "--train-ratio":
-        if float(av[2]) <= 0.0 or float(av[2]) >= 1.0:
-            raise Exception("--train-ratio argument must be a float"
-                            " ]0.0, 1.0[")
-        option['ratio'] = float(av[2])
-    if av[3].strip() == "--seed":
-        if int(av[4]) <= 0:
-            raise Exception("--seed argument must be a positive integer")
-        option['seed'] = int(av[4])
-    if av[3].strip() == "--train-ratio":
-        if float(av[4]) <= 0.0 or float(av[4]) >= 1.0:
-            raise Exception("--train-ratio argument must be a float"
-                            " ]0.0, 1.0[")
-        option['ratio'] = float(av[4])
-    return option
+    # Add parser option
+    parser.add_argument("--seed", "-s", type=int, default=1,
+                        help="Random seed for shufling.")
+    parser.add_argument("--train-ratio", "-r", type=float, default=0.8,
+                        help="ratio of the training set size.")
+    parser.add_argument("dataset_path", help="Path to the input csv file.")
+    parser.add_argument("--outfile", "-o", type=str,
+                        default="data_training.csv,data_validation.csv",
+                        help="output dataset name", nargs='+')
+
+    # Parse program arguments
+    args = parser.parse_args()
+
+    # Custom validation part
+    if not (0.0 < args.train_ratio < 1.0):
+        parser.error("The --training argument must be a float between 0.0 "
+                     "and 1.0")
+    if not (0 < args.seed):
+        parser.error("The --seed argument must be a positive integer")
+
+    if len(args.outfile) == 1:
+        outfiles = args.outfile[0].split(',')
+        outfiles = [file.strip() for file in outfiles if file.strip() != ""]
+        if not len(outfiles) == 2:
+            parser.error("The --outfile argument must contain 2 filenames.")
+        if outfiles[0] == outfiles[1]:
+            parser.error("The --outfile argument must contain 2 different "
+                         "filenames.")
+    else:
+        args.outfile = [file.strip() for file in args.outfile if
+                        file.strip() != ""]
+        if not len(args.outfile) == 2:
+            parser.error("The --outfile argument must contain 2 filenames.")
+        if args.outfile[0] == args.outfile[1]:
+            parser.error("The --outfile argument must contain 2 different "
+                         "filenames.")
+
+    return args
 
 
-def main(ac: int, av: list):
+def main(args):
     """Split the dataset to get train and validation dataset
 
     Name
@@ -106,29 +111,26 @@ def main(ac: int, av: list):
 
       --seed <int>        Random seed (default: 1)
       --train-ratio <float>  Ratio of training dataset (default: 0.8)
+      --outfiles <str,str> or <str> <str>   Output files
     """
     try:
-        if ac % 2 != 0 or (ac < 2 and ac > 6):
-            raise Exception("Usage: python split_dataset.py [OPTION] "
-                            "<dataset>")
-        # Get program options
-        options = parser(ac, av)
-
+        print(args)
         # Get Dataframe
-        df = pd.read_csv(av[-1])
+        df = pd.read_csv(args.dataset_path)
         df.columns = data_columns_names
 
         # Splitting
-        train_set = df.sample(frac=options['ratio'],
-                              random_state=options['seed'])
+        train_set = df.sample(frac=args.train_ratio,
+                              random_state=args.seed)
         validation_set = df.drop(train_set.index)
 
         # Wrinting to file
-        train_set.to_csv("data_training.csv", index=False)
-        validation_set.to_csv("data_validation.csv", index=False)
+        train_set.to_csv(args.outfile[0], index=False)
+        validation_set.to_csv(args.outfile[1], index=False)
     except Exception as e:
         print(f"{RED}Error{RESET}: {e}")
 
 
 if __name__ == "__main__":
-    main(len(sys.argv), sys.argv)
+    args = parse_args()
+    main(args)
